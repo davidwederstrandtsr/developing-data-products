eb7df93b627fc64befd2ceaceb4f89157f3a0508
davidwederstrandtsr <david.wederstrandtsr@gmail.com>
2020-06-16 16:53:15 -0500

    updating all files

--- server.py
+++ server.py
@@ -19,18 +19,27 @@ def another_page():
 def sayhello(name):
     return f'Hello, {name}!'
 
-@app.route('/order-pizza')
-def show_order_pizza_form():
-    return render_template('order-pizza-form.html')
+# my-first-form application
+@app.route('/my-first-form')
+def my_first_form():
+    return render_template('my-first-form.html')
 
-@app.route('/order-pizza', methods=['POST'])
-def handle_pizza_order_submission():
-    topping1 = request.form['topping1']
-    topping2 = request.form['topping2']
-    pizza = f'{topping1} and {topping2}'
+@app.route('/make-greeting', methods=['POST'])
+def handle_form_submission():
+    name = request.form['name']
+    title = request.form['title']
 
-    return f'One pizza with {pizza} on the way!'
+    greeting = 'Hello, '
 
+    if title != '':
+        greeting += title + ' '
+
+    greeting += name + '!'
+
+    return render_template('greeting-result.html', greeting=greeting)
+
+
+# spam-or-ham application
 @app.route('/spam-or-ham')
 def show_spam_or_ham():
     return render_template('spam-or-ham.html')
